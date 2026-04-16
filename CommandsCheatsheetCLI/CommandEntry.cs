c66656a471c837a2003a1fcb2c222f5650a8f60b
Leon <thrashing@laggard.de>
2026-04-16 18:36:31 +0200

    fix: fix?

--- CommandsCheatsheetCLI/CommandEntry.cs
+++ CommandsCheatsheetCLI/CommandEntry.cs
@@ -1,3 +1,8 @@
 ﻿namespace xyCmdCheatSheet;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Title"></param>
+/// <param name="Code"></param>
 internal record CommandEntry(string Title, string Code);
